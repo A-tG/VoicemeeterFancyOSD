dddc8a52da26654223a5288f90ead95b794c433b
A-tG <a3tg.developer@gmail.com>
2021-09-19 04:22:28 +0300

    Added method to check if Windows version supports CreateWindowInBand API

---
+++ VoicemeeterOsdProgram/Interop/NativeMethods.Ext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace VoicemeeterOsdProgram.Interop
+{
+    public static partial class NativeMethods
+    {
+        public static bool IsBandWindowSupported()
+        {
+            bool result = NativeLibrary.TryLoad("user32.dll", out IntPtr libHandle);
+            if (result)
+            {
+                result = NativeLibrary.TryGetExport(libHandle, "CreateWindowInBand", out IntPtr procHandle);
+                NativeLibrary.Free(libHandle);
+            }
+            return result;
+        }
+    }
+}
