948973a904c39613c5022ea85ca5f394aef0c43b
A-tG <a3tg.developer@gmail.com>
2021-09-21 21:59:35 +0300

    added workaround to "clean" IsParametersDirty()

--- VoicemeeterOsdProgram/Core/VoicemeeterApiClient.cs
+++ VoicemeeterOsdProgram/Core/VoicemeeterApiClient.cs
@@ -1,16 +1,17 @@
 ﻿using AtgDev.Voicemeeter;
 using AtgDev.Voicemeeter.Utils;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using System.Diagnostics;
+using System.Windows.Threading;
 
 namespace VoicemeeterOsdProgram.Core
 {
     public static class VoicemeeterApiClient
     {
+        private static DispatcherTimer m_InitTimer;
+        private static Stopwatch m_InitStopwatch;
+        private const int MaxInitTimeMs = 500;
+
         public static void Init()
         {
             AppDomain.CurrentDomain.UnhandledException += (_, _) => Logout();
@@ -18,8 +19,14 @@ namespace VoicemeeterOsdProgram.Core
             Load();
             if (IsLoaded)
             {
+                m_InitTimer = new();
+                m_InitTimer.Interval = TimeSpan.FromMilliseconds(1000 / 30);
+                m_InitTimer.Tick += OnInitTimer_Tick;
+                m_InitStopwatch = new();
+
                 Api.Login();
-                Api.IsParametersDirty();
+                m_InitStopwatch.Start();
+                m_InitTimer.Start();
             }
         }
 
@@ -30,6 +37,12 @@ namespace VoicemeeterOsdProgram.Core
         }
 
         public static bool IsLoaded
+        {
+            get;
+            private set;
+        }
+
+        public static bool IsInitialized
         {
             get;
             private set;
@@ -50,6 +63,23 @@ namespace VoicemeeterOsdProgram.Core
         public static void Logout()
         {
             Api?.Logout();
+        }
+
+        // workaround to "clean" IsParametersDirty()
+        // right after Login() it can incorrectly return 1 (New parameters) and it will trigger OSD to show
+        // so we call IsParametersDirty() in a "loop" until it returns !0 or MaxInitTimeMs is reached
+        private static void OnInitTimer_Tick(object sender, EventArgs e)
+        {
+            if ((Api.IsParametersDirty() != 0) || (m_InitStopwatch.ElapsedMilliseconds > MaxInitTimeMs))
+            {
+                m_InitStopwatch.Stop();
+                m_InitStopwatch.Stop();
+                m_InitTimer.Tick -= OnInitTimer_Tick;
+                m_InitTimer = null;
+                m_InitStopwatch = null;
+
+                IsInitialized = true;
+            }
         }
     }
 }
