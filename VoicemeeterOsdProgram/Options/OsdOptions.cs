4bd9ccc472d1468350a8872b7fc69eaffc54f4b1
A-tG <a3tg.developer@gmail.com>
2021-10-06 15:51:39 +0300

    removed unused methods

--- VoicemeeterOsdProgram/Options/OsdOptions.cs
+++ VoicemeeterOsdProgram/Options/OsdOptions.cs
@@ -11,40 +11,5 @@ namespace VoicemeeterOsdProgram.Options
         public bool IsShowOnlyIfVoicemeeterHidden { get; set; } = true;
         public bool IsInteractable { get; set; } = false;
         public int DurationMs { get; set; } = 2000;
-
-        public void ToIniData(ref IniData data)
-        {
-            var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var prop in properties)
-            {
-                data[SectionName][prop.Name] = prop.GetValue(this).ToString();
-            }
-        }
-
-        public void FromIniData(ref IniData data)
-        {
-            var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var prop in properties)
-            {
-                ValidateOption(ref data, prop);
-            }
-        }
-
-        private void ValidateOption(ref IniData data, PropertyInfo prop)
-        {
-            var name = prop.Name;
-            var field = data[SectionName][name];
-            try
-            {
-                var result = Convert.ChangeType(field, prop.PropertyType);
-                if (result is not null)
-                {
-                    prop.SetValue(this, result);
-                    return;
-                }
-            }
-            catch { }
-            data[SectionName][name] = prop.GetValue(this).ToString();
-        }
     }
 }
