152c3293e237929dcafc006cbb6e0c37dd0250e4
A-tG <a3tg.developer@gmail.com>
2021-10-06 15:52:15 +0300

    Update OsdOptions.cs

--- VoicemeeterOsdProgram/Options/OsdOptions.cs
+++ VoicemeeterOsdProgram/Options/OsdOptions.cs
@@ -1,13 +1,7 @@
-﻿using IniParser.Model;
-using System;
-using System.Reflection;
-
-namespace VoicemeeterOsdProgram.Options
+﻿namespace VoicemeeterOsdProgram.Options
 {
     public class OsdOptions
     {
-        private const string SectionName = "Osd";
-
         public bool IsShowOnlyIfVoicemeeterHidden { get; set; } = true;
         public bool IsInteractable { get; set; } = false;
         public int DurationMs { get; set; } = 2000;
