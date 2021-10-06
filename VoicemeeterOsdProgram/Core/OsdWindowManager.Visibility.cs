de6de61f6cf52a5b3d26cc4d0a700ecc55a6ea77
A-tG <a3tg.developer@gmail.com>
2021-10-06 17:30:12 +0300

    refactoring

---
+++ VoicemeeterOsdProgram/Core/OsdWindowManager.Visibility.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+using VoicemeeterOsdProgram.UiControls.OSD.Strip;
+
+namespace VoicemeeterOsdProgram.Core
+{
+    partial class OsdWindowManager
+    {
+        private static void UpdateOsdElementsVis()
+        {
+            m_wpfControl.AllowAutoUpdateSeparators = false;
+
+            var children = m_wpfControl.MainContent.Children;
+            foreach (StripControl strip in children)
+            {
+                bool isAnyVisibleBtn = false;
+                foreach (ButtonContainer btnCont in strip.BusBtnsContainer.Children)
+                {
+                    if (btnCont.Visibility == Visibility.Visible)
+                    {
+                        isAnyVisibleBtn = true;
+                        break;
+                    }
+                }
+                foreach (ButtonContainer btnCont in strip.ControlBtnsContainer.Children)
+                {
+                    if (btnCont.Visibility == Visibility.Visible)
+                    {
+                        isAnyVisibleBtn = true;
+                        break;
+                    }
+                }
+
+                bool isVisibleChildren = (strip.FaderCont.Visibility == Visibility.Visible) || isAnyVisibleBtn;
+                if (isVisibleChildren)
+                {
+                    strip.Visibility = Visibility.Visible;
+                }
+            }
+
+            m_wpfControl.UpdateSeparators();
+            m_wpfControl.AllowAutoUpdateSeparators = true;
+        }
+
+        private static void ApplyVisibilityToOsdElements(Visibility vis)
+        {
+            m_wpfControl.AllowAutoUpdateSeparators = false;
+
+            var children = m_wpfControl.MainContent.Children;
+            foreach (StripControl strip in children)
+            {
+                strip.Visibility = vis;
+                strip.FaderCont.Visibility = vis;
+                foreach (ButtonContainer btnCont in strip.BusBtnsContainer.Children)
+                {
+                    btnCont.Visibility = vis;
+                }
+                foreach (ButtonContainer btnCont in strip.ControlBtnsContainer.Children)
+                {
+                    btnCont.Visibility = vis;
+                }
+            }
+
+            m_wpfControl.UpdateSeparators();
+            m_wpfControl.AllowAutoUpdateSeparators = true;
+        }
+    }
+}
--- VoicemeeterOsdProgram/Core/OsdWindowManager.cs
+++ VoicemeeterOsdProgram/Core/OsdWindowManager.cs
@@ -100,7 +100,8 @@ namespace VoicemeeterOsdProgram.Core
             private set;
         }
 
-        public static bool IsIgnoreVmParameters => m_isMouseEntered || OptionsStorage.Osd.IsShowOnlyIfVoicemeeterHidden && IsVoicemeeterWindowForeground();
+        public static bool IsIgnoreVmParameters =>
+            m_isMouseEntered || OptionsStorage.Osd.IsShowOnlyIfVoicemeeterHidden && IsVoicemeeterWindowForeground();
 
         public static void Show()
         {
@@ -178,66 +179,7 @@ namespace VoicemeeterOsdProgram.Core
             const string WindowText = "VoiceMeeter";
 
             IntPtr hWnd = FindWindowEx(IntPtr.Zero, IntPtr.Zero, WindowClass, WindowText);
-            return !WindowObstructedHelper.IsObstructed(hWnd); ;
-        }
-
-        private static void UpdateOsdElementsVis()
-        {
-            m_wpfControl.AllowAutoUpdateSeparators = false;
-
-            var children = m_wpfControl.MainContent.Children;
-            foreach (StripControl strip in children)
-            {
-                bool isAnyVisibleBtn = false;
-                foreach (ButtonContainer btnCont in strip.BusBtnsContainer.Children)
-                {
-                    if (btnCont.Visibility == Visibility.Visible)
-                    {
-                        isAnyVisibleBtn = true;
-                        break;
-                    }
-                }
-                foreach (ButtonContainer btnCont in strip.ControlBtnsContainer.Children)
-                {
-                    if (btnCont.Visibility == Visibility.Visible)
-                    {
-                        isAnyVisibleBtn = true;
-                        break;
-                    }
-                }
-
-                bool isVisibleChildren = (strip.FaderCont.Visibility == Visibility.Visible) || isAnyVisibleBtn;
-                if (isVisibleChildren)
-                {
-                    strip.Visibility = Visibility.Visible;
-                }
-            }
-
-            m_wpfControl.UpdateSeparators();
-            m_wpfControl.AllowAutoUpdateSeparators = true;
-        }
-
-        private static void ApplyVisibilityToOsdElements(Visibility vis)
-        {
-            m_wpfControl.AllowAutoUpdateSeparators = false;
-
-            var children = m_wpfControl.MainContent.Children;
-            foreach (StripControl strip in children)
-            {
-                strip.Visibility = vis;
-                strip.FaderCont.Visibility = vis;
-                foreach (ButtonContainer btnCont in strip.BusBtnsContainer.Children)
-                {
-                    btnCont.Visibility = vis;
-                }
-                foreach (ButtonContainer btnCont in strip.ControlBtnsContainer.Children)
-                {
-                    btnCont.Visibility = vis;
-                }
-            }
-
-            m_wpfControl.UpdateSeparators();
-            m_wpfControl.AllowAutoUpdateSeparators = true;
+            return !WindowObstructedHelper.IsObstructed(hWnd);
         }
 
         private static void OnNewVoicemeeterParams(object sender, EventArgs e)
