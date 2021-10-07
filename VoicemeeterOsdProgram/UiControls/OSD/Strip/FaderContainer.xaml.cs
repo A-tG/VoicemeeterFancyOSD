0a5e24501691680e54a769524a8ea7362726b1b9
A-tG <a3tg.developer@gmail.com>
2021-10-08 01:02:00 +0300

    added Mousewheel and DoubleClick fader manipulation

--- VoicemeeterOsdProgram/UiControls/OSD/Strip/FaderContainer.xaml.cs
+++ VoicemeeterOsdProgram/UiControls/OSD/Strip/FaderContainer.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace VoicemeeterOsdProgram.UiControls.OSD.Strip
 {
@@ -10,6 +11,33 @@ namespace VoicemeeterOsdProgram.UiControls.OSD.Strip
         public FaderContainer()
         {
             InitializeComponent();
+        }
+
+        private void OnFaderMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var slider = sender as Slider;
+            if (sender is null) return;
+
+            double val = 3;
+            if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+            {
+                val = 1;
+            }
+            else if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+            {
+                val = 0.1;
+            }
+            if (e.Delta < 0) val *= -1;
+
+            slider.Value += val;
+        }
+
+        private void OnFaderMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var slider = sender as Slider;
+            if (sender is null) return;
+
+            slider.Value = 0;
         }
     }
 }
