eaf663325e7fd7756ecd1471771c5a54ce772467
A-tG <a3tg.developer@gmail.com>
2021-10-12 00:43:04 +0300

    using expression body

--- VoicemeeterOsdProgram/Core/Types/VoicemeeterNumParam.cs
+++ VoicemeeterOsdProgram/Core/Types/VoicemeeterNumParam.cs
@@ -1,5 +1,4 @@
 ﻿using AtgDev.Voicemeeter;
-using System;
 
 namespace VoicemeeterOsdProgram.Core.Types
 {
@@ -7,19 +6,8 @@ namespace VoicemeeterOsdProgram.Core.Types
     {
         public VoicemeeterNumParam(RemoteApiExtender api, string command) : base(api, command) { }
 
-        public override int GetParameter(out float val)
-        {
-            return m_api.GetParameter(m_command, out val);
-        }
+        public override int GetParameter(out float val) => m_api.GetParameter(m_command, out val);
 
-        public void Write(float value)
-        {
-            if ((m_api is null) || string.IsNullOrEmpty(m_command)) return;
-
-            if (m_api.SetParameter(m_command, value) == 0)
-            {
-                m_value = value;
-            }
-        }
+        public override int SetParameter(float value) => m_api.SetParameter(m_command, value);
     }
 }
