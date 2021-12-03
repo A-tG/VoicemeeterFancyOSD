8d50d0f4f23134bc61ee5d6a6e2d0d990b648c5e
A-tG <a3tg.developer@gmail.com>
2021-12-03 10:28:41 +0300

    base class for Options

---
+++ VoicemeeterOsdProgram/Options/OptionsBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoicemeeterOsdProgram.Options
+{
+    public abstract class OptionsBase
+    {
+        protected void HandlePropertyChange<T>(ref T oldVal, ref T newVal, EventHandler<T> eventIfNotEqual)
+        {
+            if (oldVal.Equals(newVal)) return;
+
+            oldVal = newVal;
+            eventIfNotEqual?.Invoke(this, newVal);
+        }
+    }
+}
