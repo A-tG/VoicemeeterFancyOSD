1abf275c11bf02bb4ac6ceaf50896a896cfa7278
A-tG <a3tg.developer@gmail.com>
2021-12-04 13:05:13 +0300

    Added async method to extract archive

---
+++ VoicemeeterOsdProgram/Helpers/ZipFileExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtgDev.Utils.Extensions
+{
+    public static class ZipFileExntesions
+    {
+        public static async Task ExtractToDirectoryAsync(string sourceArchiveFileName, string destinationDirectoryName)
+        {
+            using ZipArchive archive = ZipFile.OpenRead(sourceArchiveFileName);
+            foreach (var entry in archive.Entries)
+            {
+                string path = Path.GetFullPath(Path.Combine(destinationDirectoryName, entry.FullName));
+                bool isDirectory = string.IsNullOrEmpty(entry.Name);
+                if (isDirectory)
+                {
+                    Directory.CreateDirectory(path);
+                }
+                else
+                {
+                    await using Stream inStream = entry.Open();
+                    await using Stream outStream = File.Create(path);
+                    await ProcessStreams(inStream, outStream);
+                }
+            }
+        }
+
+        private static async Task ProcessStreams(Stream input, Stream output)
+        {
+            byte[] buffer = new byte[4096];
+            int bytesRead = 0;
+            do
+            {
+                bytesRead = await input.ReadAsync(buffer, 0, buffer.Length);
+                await output.WriteAsync(buffer, 0, bytesRead);
+            } while (bytesRead > 0);
+        }
+    }
+}
