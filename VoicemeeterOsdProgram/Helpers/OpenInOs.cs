2e1bea9f9cf0ed2812af00e58eb7782348662af2
A-tG <a3tg.developer@gmail.com>
2021-12-08 01:57:57 +0300

    added OpenInOs helper class

---
+++ VoicemeeterOsdProgram/Helpers/OpenInOs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace AtgDev.Utils
+{
+    public static class OpenInOs
+    {
+        public static bool TryOpen(string path)
+        {
+            bool result = false;
+            try
+            {
+                using Process p = new();
+                ProcessStartInfo info = new()
+                {
+                    FileName = path,
+                    UseShellExecute = true
+                };
+                p.StartInfo = info;
+                //p.StartInfo.Arguments = $@"""{path}""";
+                _ = p.Start();
+
+                result = true;
+            }
+            catch { }
+            return result;
+        }
+
+        private static string GetProcName()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return "explorer.exe";
+            }
+            else
+            {
+                throw new PlatformNotSupportedException();
+            }
+        }
+    }
+}
