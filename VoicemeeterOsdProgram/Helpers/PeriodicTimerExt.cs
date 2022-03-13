c1d60cc47fd24ba3a7ca1e9d522a2165d892e1c1
A-tG <a3tg.developer@gmail.com>
2022-03-13 07:07:53 +0300

    added wrapper for PeriodicTimer

---
+++ VoicemeeterOsdProgram/Helpers/PeriodicTimerExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AtgDev.Utils
+{
+    public class PeriodicTimerExt
+    {
+        private PeriodicTimer m_timer;
+
+        public TimeSpan period;
+
+        public void Start()
+        {
+            m_timer = new(period);
+        }
+
+        public void Stop()
+        {
+            m_timer.Dispose();
+            m_timer = null;
+        }
+
+        public async ValueTask<bool> WaitForNextTickAsync() => await m_timer.WaitForNextTickAsync();
+    }
+}
