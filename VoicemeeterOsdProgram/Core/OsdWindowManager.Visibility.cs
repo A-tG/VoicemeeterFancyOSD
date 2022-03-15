5f7c61f9fe694b446940e4f3dbff0547d5eface2
A-tG <a3tg.developer@gmail.com>
2022-03-15 22:09:13 +0300

    removed using

--- VoicemeeterOsdProgram/Core/OsdWindowManager.Visibility.cs
+++ VoicemeeterOsdProgram/Core/OsdWindowManager.Visibility.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using VoicemeeterOsdProgram.UiControls.OSD.Strip;
-using VoicemeeterOsdProgram.Factories;
 
 namespace VoicemeeterOsdProgram.Core
 {
