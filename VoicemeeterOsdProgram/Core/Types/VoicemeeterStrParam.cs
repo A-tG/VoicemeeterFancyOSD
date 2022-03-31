a347ce1bd9a5dfbfd4823d52e4064f937969af02
A-tG <a3tg.developer@gmail.com>
2022-03-31 15:29:10 +0300

    replaced AllocHGlobal()

--- VoicemeeterOsdProgram/Core/Types/VoicemeeterStrParam.cs
+++ VoicemeeterOsdProgram/Core/Types/VoicemeeterStrParam.cs
@@ -1,36 +1,34 @@
 ﻿using AtgDev.Voicemeeter;
 using System;
-using System.Runtime.InteropServices;
 
 namespace VoicemeeterOsdProgram.Core.Types
 {
     public class VoicemeeterStrParam : VoicemeeterParameterBase<string>
     {
-        protected readonly IntPtr m_strGetValueBuffer;
+        protected readonly char[] m_strGetValueBuffer = new char[512];
 
         public VoicemeeterStrParam(RemoteApiExtender api, string command) : base(api, command)
         {
             m_value = string.Empty;
             m_isInit = true;
-            m_strGetValueBuffer = Marshal.AllocHGlobal(1024);
         }
 
-        public override int GetParameter(out string val)
+        unsafe public override int GetParameter(out string val)
         {
             val = "";
-            var res = m_api.GetParameter(m_nameBuffer, m_strGetValueBuffer);
-            if (res == ResultCodes.Ok)
+            fixed (char* buffPtr = m_strGetValueBuffer)
             {
-                val = Marshal.PtrToStringUni(m_strGetValueBuffer) ?? "";
+                var ptr = (IntPtr)buffPtr;
+
+                var res = m_api.GetParameter(m_nameBuffer, ptr);
+                if (res == ResultCodes.Ok)
+                {
+                    val = new string(buffPtr);
+                }
+                return res;
             }
-            return res;
         }
 
         public override int SetParameter(string value) => m_api.SetParameter(m_nameBuffer, value);
-
-        ~VoicemeeterStrParam()
-        {
-            Marshal.FreeHGlobal(m_strGetValueBuffer);
-        }
     }
 }
