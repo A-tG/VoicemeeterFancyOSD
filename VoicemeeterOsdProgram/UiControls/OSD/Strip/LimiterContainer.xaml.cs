91c082642ee487214fc999c264d4eec8601a28e8
A-tG <a3tg.developer@gmail.com>
2022-05-03 05:40:47 +0300

    Added Limiter slider

---
+++ VoicemeeterOsdProgram/UiControls/OSD/Strip/LimiterContainer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VoicemeeterOsdProgram.UiControls.OSD.Strip
+{
+    /// <summary>
+    /// Interaction logic for LimiterContainer.xaml
+    /// </summary>
+    public partial class LimiterContainer : ContentControl
+    {
+        public LimiterContainer()
+        {
+            InitializeComponent();
+        }
+    }
+}
