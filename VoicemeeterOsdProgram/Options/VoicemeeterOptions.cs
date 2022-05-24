aa05e3982ffd30937b55c00b8db2ac3414ea1fe7
A-tG <a3tg.developer@gmail.com>
2022-05-24 20:46:06 +0300

    added InitializationDelay option

--- VoicemeeterOsdProgram/Options/VoicemeeterOptions.cs
+++ VoicemeeterOsdProgram/Options/VoicemeeterOptions.cs
@@ -7,6 +7,7 @@ namespace VoicemeeterOsdProgram.Options
     public class VoicemeeterOptions : OptionsBase
     {
         private VoicemeeterApiClient.Rate m_apiPoolingRate = VoicemeeterApiClient.Rate.Normal;
+        private uint m_initDelay = 0;
 
         [Description("How fast Voicemeeter API is called to to track changes. Recommended: Normal or Fast. Faster rate - more cpu usage and OSD is more responsive to quick changes. Options represent 15, 30, 60, 144hz")]
         public VoicemeeterApiClient.Rate ApiPoolingRate
@@ -15,6 +16,14 @@ namespace VoicemeeterOsdProgram.Options
             set => HandlePropertyChange(ref m_apiPoolingRate, ref value, ApiPoolingRateChanged);
         }
 
+        [Description("Wait ms before loading/connecting to Voicemeeter Remote API. Might help if program experience problems with vmrapi on system startup")]
+        public uint InitializationDelay
+        {
+            get => m_initDelay;
+            set => HandlePropertyChange(ref m_initDelay, ref value, DelayInitializationChanged);
+        }
+
         public event EventHandler<VoicemeeterApiClient.Rate> ApiPoolingRateChanged;
+        public event EventHandler<uint> DelayInitializationChanged;
     }
 }
