4e6747f555572bcbe7596bd2d5ff3f872f5fa2e8
A-tG <a3tg.developer@gmail.com>
2022-06-02 05:59:12 +0300

    added class for simple Dialog window

---
+++ VoicemeeterOsdProgram/UiControls/Dialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VoicemeeterOsdProgram.UiControls
+{
+    /// <summary>
+    /// Interaction logic for Dialog.xaml
+    /// </summary>
+    public partial class Dialog : Window
+    {
+        public Dialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e) => Close();
+    }
+}
