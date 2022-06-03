60b23069f20cb4128b09fdad0abb2974731035ea
A-tG <a3tg.developer@gmail.com>
2022-06-03 18:04:38 +0300

    added ThumbExt() class

---
+++ VoicemeeterOsdProgram/UiControls/ThumbExt.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace VoicemeeterOsdProgram.UiControls
+{
+    public class ThumbExt : Thumb
+    {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value), typeof(double), typeof(ThumbExt));
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+    }
+}
