a58aa511c8f87c35a74eb98a307eae0d2f6adf02
A-tG <a3tg.developer@gmail.com>
2022-06-10 02:51:22 +0300

    added InputOption usercontrol

---
+++ VoicemeeterOsdProgram/UiControls/Settings/InputOption.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace VoicemeeterOsdProgram.UiControls.Settings
+{
+    /// <summary>
+    /// Interaction logic for InputOption.xaml
+    /// </summary>
+    public partial class InputOption : UserControl
+    {
+        public Func<string, bool> filterTextFunc = (_) => true;
+
+        public InputOption()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(InputValue), typeof(string), typeof(InputOption));
+        public string InputValue
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        private void PreviewText(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !filterTextFunc(e.Text);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(InputValue);
+        }
+    }
+}
---
+++ VoicemeeterOsdProgram/UiControls/Settings/InputOptionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using VoicemeeterOsdProgram.Types;
+
+namespace VoicemeeterOsdProgram.UiControls.Settings
+{
+    public class InputOptionViewModel: BaseViewModel
+    {
+        private string m_label = "";
+
+        public string Label
+        {
+            get => m_label;
+            set
+            {
+                m_label = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
