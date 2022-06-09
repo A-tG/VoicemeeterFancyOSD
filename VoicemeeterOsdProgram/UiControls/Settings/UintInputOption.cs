e5714c31784552d5f8933248e3926d26b2352d4a
A-tG <a3tg.developer@gmail.com>
2022-06-10 02:51:28 +0300

    Create UintInputOption.cs

---
+++ VoicemeeterOsdProgram/UiControls/Settings/UintInputOption.cs
@@ -0,0 +1,19 @@
+﻿namespace VoicemeeterOsdProgram.UiControls.Settings
+{
+    public class UintInputOption : InputOption
+    {
+        public UintInputOption() : base()
+        {
+            filterTextFunc = IsOnlyDigits;
+        }
+
+        private bool IsOnlyDigits(string text)
+        {
+            foreach (var ch in text)
+            {
+                if (!char.IsDigit(ch)) return false;
+            }
+            return true;
+        }
+    }
+}
