74b02027d4bd258dd89c405024714de9a710a9d3
A-tG <a3tg.developer@gmail.com>
2022-06-10 08:03:53 +0300

    small refactoring

--- VoicemeeterOsdProgram/UiControls/Helpers/PanelMarginSetter.cs
+++ VoicemeeterOsdProgram/UiControls/Helpers/PanelMarginSetter.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
-namespace VoicemeeterOsdProgram.UiControls.Settings
+namespace VoicemeeterOsdProgram.UiControls.Helpers
 {
     public class PanelMarginSetter
     {
