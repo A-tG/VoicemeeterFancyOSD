0be0380ba623398dea4bce84e8f2016e772cd3c8
A-tG <a3tg.developer@gmail.com>
2022-06-10 10:56:08 +0300

    added Voicemeeter

--- VoicemeeterOsdProgram/UiControls/Settings/SettingsWindow.xaml.cs
+++ VoicemeeterOsdProgram/UiControls/Settings/SettingsWindow.xaml.cs
@@ -50,6 +50,10 @@ namespace VoicemeeterOsdProgram.UiControls.Settings
             item = new OptionsSection();
             item.ContentContainer.Content = new Logger();
             ch.Add(item);
+
+            item = new OptionsSection();
+            item.ContentContainer.Content = new Voicemeeter();
+            ch.Add(item);
         }
 
         private void SettingsWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
