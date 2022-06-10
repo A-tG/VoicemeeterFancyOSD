b682e573b0395b1e688480021b2668fbf18e645e
A-tG <a3tg.developer@gmail.com>
2022-06-10 08:06:03 +0300

    data binding usage

--- VoicemeeterOsdProgram/UiControls/Settings/Program.xaml.cs
+++ VoicemeeterOsdProgram/UiControls/Settings/Program.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
 using VoicemeeterOsdProgram.Options;
@@ -10,7 +8,7 @@ namespace VoicemeeterOsdProgram.UiControls.Settings
     /// <summary>
     /// Interaction logic for Program.xaml
     /// </summary>
-    public partial class Program : StackPanel
+    public partial class Program : UserControl
     {
         public Dictionary<RenderMode, string> RmodeValues { get; } = new()
         {
@@ -20,39 +18,10 @@ namespace VoicemeeterOsdProgram.UiControls.Settings
 
         public Program()
         {
+            var o = OptionsStorage.Program;
+            DataContext = o;
+
             InitializeComponent();
-
-            var o = OptionsStorage.Program;
-
-            AutostartChbox.IsChecked = o.Autostart;
-            RenderingModeCombo.SelectedValue = o.RenderMode;
-
-            o.AutostartChanged += OptionEvent_Autostart;
-            o.RenderModeChanged += OptionEvent_RenderMode;
-
-            AutostartChbox.Click += (_, _) => o.Autostart = AutostartChbox.IsChecked ?? false;
-            RenderingModeCombo.SelectionChanged += RenderingModeSelected;
-
-            Unloaded += OnUnload;
         }
-
-        private void OnUnload(object sender, RoutedEventArgs e)
-        {
-            var o = OptionsStorage.Program;
-            o.AutostartChanged -= OptionEvent_Autostart;
-            o.RenderModeChanged -= OptionEvent_RenderMode;
-        }
-
-        private void RenderingModeSelected(object sender, SelectionChangedEventArgs e)
-        {
-            var items = e.AddedItems.OfType<KeyValuePair<RenderMode, string>>().ToArray();
-            if (items.Length == 0) return;
-
-            OptionsStorage.Program.RenderMode = items[0].Key;
-        }
-
-        private void OptionEvent_Autostart(object o, bool val) => AutostartChbox.IsChecked = val;
-
-        private void OptionEvent_RenderMode(object o, RenderMode val) => RenderingModeCombo.SelectedValue = val;
     }
 }
