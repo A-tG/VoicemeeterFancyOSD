02841cae7a9698a10fd586066541103ab4f0c526
A-tG <a3tg.developer@gmail.com>
2022-06-12 03:13:42 +0300

    Added About page

---
+++ VoicemeeterOsdProgram/UiControls/Settings/AboutViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using VoicemeeterOsdProgram.Types;
+
+namespace VoicemeeterOsdProgram.UiControls.Settings
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public string Version
+        {
+            get => Assembly.GetEntryAssembly().GetName().Version.ToString();
+            set
+            {
+                return;
+            }
+        }
+    }
+}
