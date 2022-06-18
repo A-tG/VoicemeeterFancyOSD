6ea4f061a966284a1dbad6896f401adffa4b6f6f
A-tG <a3tg.developer@gmail.com>
2022-06-18 14:36:47 +0300

    added PopupExt class

---
+++ VoicemeeterOsdProgram/UiControls/PopupExt.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls.Primitives;
+
+namespace VoicemeeterOsdProgram.UiControls
+{
+    public class PopupExt : Popup
+    {
+        public PopupExt()
+        {
+            MouseDown += (_, e) => e.Handled = true; 
+        }
+    }
+}
