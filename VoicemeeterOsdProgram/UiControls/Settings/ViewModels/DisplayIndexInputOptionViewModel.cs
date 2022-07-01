c9624ea4194fa53f650e88d7e0fdec1153b5f437
A-tG <a3tg.developer@gmail.com>
2022-07-01 21:07:50 +0300

    better name and description

--- VoicemeeterOsdProgram/UiControls/Settings/ViewModels/DisplayIndexInputOptionViewModel.cs
+++ VoicemeeterOsdProgram/UiControls/Settings/ViewModels/DisplayIndexInputOptionViewModel.cs
@@ -4,8 +4,8 @@
     {
         public DisplayIndexInputOptionViewModel()
         {
-            Label = "Display used index";
-            TooltipText = "0 - is a primary display, 1 - is a secondary, etc";
+            Label = "Display";
+            TooltipText = "The index of the display being used.\n0 - is a primary display, 1 - is a secondary, etc";
         }
     }
 }
