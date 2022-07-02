fac12467115f6b4405e7ec98879a4806f181db9e
A-tG <a3tg.developer@gmail.com>
2022-07-02 18:30:59 +0300

    added checkbox container

---
+++ VoicemeeterOsdProgram/UiControls/Settings/CheckboxWrap.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VoicemeeterOsdProgram.UiControls.Settings
+{
+    /// <summary>
+    /// Interaction logic for CheckboxWrap.xaml
+    /// </summary>
+    public partial class CheckboxWrap : UserControl
+    {
+        public CheckboxWrap()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.Register(
+            nameof(IsChecked), typeof(bool), typeof(CheckboxWrap));
+
+        public bool IsChecked
+        {
+            get => (bool)GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+    }
+}
---
+++ VoicemeeterOsdProgram/UiControls/Settings/ViewModels/CheckboxWrapViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoicemeeterOsdProgram.Types;
+
+namespace VoicemeeterOsdProgram.UiControls.Settings.ViewModels
+{
+    public class CheckboxWrapViewModel : BaseViewModel
+    {
+        private object m_content, m_tooltipContent;
+
+        public object Content
+        {
+            get => m_content;
+            set
+            {
+                m_content = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public object TooltipContent
+        {
+            get => m_tooltipContent;
+            set
+            {
+                m_tooltipContent = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
