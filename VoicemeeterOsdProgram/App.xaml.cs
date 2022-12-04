dcb11f457bc221b97cdef03011b8da36521522af
A-tG <a3tg.developer@gmail.com>
2022-12-04 04:51:02 +0300

    small refactoring

--- VoicemeeterOsdProgram/App.xaml.cs
+++ VoicemeeterOsdProgram/App.xaml.cs
@@ -26,8 +26,7 @@ namespace VoicemeeterOsdProgram
         async void OnAppStartup(object sender, StartupEventArgs e)
         {
             var optionsTask = OptionsStorage.InitAsync();
-
-            RenderOptions.ProcessRenderMode = OptionsStorage.Program.RenderMode;
+            OptionsStorage.Program.RenderMode = RenderOptions.ProcessRenderMode;
             OptionsStorage.Program.RenderModeChanged += (_, val) => RenderOptions.ProcessRenderMode = val;
 
             VoicemeeterApiClient.PoolingRate = OptionsStorage.Voicemeeter.ApiPollingRate;
